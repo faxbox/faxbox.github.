58537faa6549a0694bec5e190549492724b8908a
faxbox <118101077+faxbox@users.noreply.github.com>
2022-11-22 13:38:58 +0800

    Delete api directory

--- api/user/getMsg.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once("../../fn/mysqli.php");
-		$arrWhere=['user_id'=>$uid];
-		$arr=['user_nick','user_icon'];
-		$result= mysql_get('user',$arr,$arrWhere,null);
-		echo '{"result":"success","msg":'.$result.'}';
-?>
